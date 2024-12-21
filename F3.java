16cf96776e2783eb3db4290099c0c53b50ecf06d
Sumit Kanchole <kancholesumit@gmail.com>
2024-12-21 11:51:47 -0800

    Update F3.java
    update some mistake of this code

--- F3.java
+++ F3.java
@@ -1,55 +1,57 @@
 import java.util.*;
-class digit3 {
-
-void digitCount(int num) {
-	
-	int sum=0,count=0,rem=0;
-	int x=num;
-	while(num!=0) {
-		
-		int r = num/10;
-		sum+=r;
+class Power{
+	void getNum(int num) {
+		int count=0,add=0,sum=0;
+		int x=num;
+		while(num!=0) {
+		num = num/10;
+		add+=num;
 		count++;
 		}
-	int ct = count;
-	if(ct==2){
-	while(num>0){
-	rem = num%10;
-          sum+=(rem*rem);
-          num=num/10;
-          }
-	System.out.println(sum);
-		}
-	else if(ct==3){
-	while(num>0){
-	rem = num%10;
-          sum+=(rem*rem*rem);
-          num=num/10;
-          }
-	System.out.println(sum);
-	}
-	else if(ct==4){
-	while(num>0){
-	rem = num%10;
-          sum+=(rem*rem*rem*rem);
-          num=num/10;
-          }
-	System.out.println(sum);
-	}
-	else {
-	System.out.println("Number greater than 4 ");
-	}
 
+		int temp = count;
+
+		if(temp==2){
+		while(x!=0) {
+		int y = x%10;
+		sum = sum+(y*y);
+		x = x/10;
+		}
+		System.out.println("Sum of number is : "+sum);
+		}
+
+		else if(temp==3){
+		while(x!=0) {
+		int y = x%10;
+		sum = sum+(y*y*y);
+		x = x/10;
+		}
+		System.out.println("Sum of number is : "+sum);
+		}
+
+		else if(temp==4){
+		while(x!=0) {
+		int y = x%10;
+		sum = sum+(y*y*y*y);
+		x = x/10;
+		}
+		System.out.println("Sum of number is : "+sum);
+		}
+
+		else {
+		System.out.println("your power above the 4 ");
+		}
 }
 }
 class F3 {
 public static void main(String args[]) {
 Scanner sc=new Scanner(System.in);
-System.out.print("Enter numbers : ");
-int num = sc.nextInt();
 
-digit3 obj = new digit3();
-obj.digitCount(num);
+System.out.println("Enter a number : ");
+int num =sc.nextInt();
+
+Power p1 = new Power();
+p1.getNum(num);
+
 }
-
 }
